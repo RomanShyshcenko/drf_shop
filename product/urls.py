011cc3ec37f81ddf6e9d753150a477ac261a9734
Roman Shyshchenko <shyshchenkoroman@gmail.com>
2023-08-15 20:39:27 +0300

    create 3 new path: sub_category/eneble/all/, sub_category/enable/, category/disable/ .  and rewrite 1: category/enable/without_subcategories/

--- product/urls.py
+++ product/urls.py
@@ -3,12 +3,20 @@ from product import views
 
 urlpatterns = [
     path('category/create/', views.CreateCategory.as_view(), name='create_category'),
+    path('category/disable/', views.CategoryDisableSubcategoriesView.as_view(),
+         name='disable_category_and_subcategories'),
     path(
-        'category/disable/',
-        views.CategoryDisableSubcategoriesView.as_view(),
-        name='disable_category_and_subcategories'
+        'category/enable/without_subcategories/',
+        views.ActivateCategoryWithoutSubCategoriesView.as_view(),
+        name='enable_category_without_subcategories'
     ),
     path('sub_category/create/', views.CreateSubCategory.as_view(), name='create_sub_category'),
     path('sub_category/disable/', views.DisableSubCategoryView.as_view(), name='disable_sub_category'),
+    path('sub_category/enable/', views.ActivateSubCategoryView.as_view(), name='enable_sub_category'),
+    path(
+        'sub_category/eneble/all/',
+        views.ActivateSubCategoriesOfConcreteCategoryView.as_view(),
+        name='activate_sub_categories_of_concrete_category'
+    ),
 
 ]
