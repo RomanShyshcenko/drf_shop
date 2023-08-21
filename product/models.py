ed4cc2b92d1bdda331696e36e7635e8f0b84af34
Roman Shyshchenko <shyshchenkoroman@gmail.com>
2023-08-21 18:20:10 +0300

    rewrite a dleted_at field

--- product/models.py
+++ product/models.py
@@ -43,7 +43,7 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -53,78 +53,3 @@ class Product(models.Model):
         db_table = "product"
 
 
-
-# // Use DBML to define your database structure
-# // Docs: https://dbml.dbdiagram.io/docs
-#
-# Table users {
-#   id integer
-#   uuid uuid [primary key]
-#   email varchar
-#   first_name varchar
-#   last_name varchar
-#   password varchar
-#   is_staff boolean
-#   is_active boolean
-#   is_confirmed_email boolean
-#   created_at timestamp
-#   updated_at timestamp
-# }
-#
-# Table phone_number {
-#   id integer [primary key]
-#   user_id uuid
-#   phone_number varchar
-#   is_verified boolean
-#   sent timestamp
-#   created_at timestamp
-#   updated_at timestamp
-# }
-#
-# Table user_address {
-#   id integer [primary key]
-#   user_id uuid
-#   city varchar
-#   street_address varchar
-#   apartment_address varchar
-#   postal_code varchar
-#   created_at timestamp
-#   updated_at timestamp
-# }
-#
-# Table category {
-#   id integer [primary key]
-#   name varchar
-#   is_active boolean
-#   created_at timestamp
-# }
-#
-# Table sub_category{
-#   id integer [primary key]
-#   category_id integer
-#   name varchar
-#   is_active boolean
-#   created_at timestamp
-# }
-#
-# Table product {
-#   id integer [primary key]
-#   sub_category_id integer
-#   name varchar
-#   brand varchar
-#   descripption text
-#   is_active boolean
-#   created_at timestamp
-#   updated_at timestamp
-#   deleted_at timestamp
-# }
-#
-#
-# Ref: "users"."uuid" < "phone_number"."user_id"
-#
-#
-# Ref: "users"."uuid" < "user_address"."user_id"
-#
-# Ref: "category"."id" < "sub_category"."category_id"
-#
-# Ref: "sub_category"."id" < "product"."sub_category_id"
