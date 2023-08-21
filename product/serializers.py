aad02aa91325c387f7bd5e7e8b5672a8e500cc13
Roman Shyshchenko <shyshchenkoroman@gmail.com>
2023-08-21 18:17:10 +0300

    add sezializers for product

--- product/serializers.py
+++ product/serializers.py
@@ -1,16 +1,34 @@
 from django.utils import timezone
 from rest_framework import serializers, status
-from rest_framework.serializers import ModelSerializer, Serializer
+from rest_framework.serializers import ModelSerializer
 
 from product.models import Product, Category, SubCategory
 
 
-class ProductSerializer(ModelSerializer):
+class CreateProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'category_id', 'name', 'brand', 'description', 'is_active',
-                  'created_at', 'updated_at', 'deleted_at')
-        read_only_fields = ('id', 'category_id', 'created_at')
+        fields = (
+            'id', 'category_id', 'name', 'brand',
+            'description', 'is_active', 'created_at'
+        )
+        read_only_fields = ('id', 'created_at')
+
+    def validate(self, attrs):
+        cat_id = attrs.get('category_id').id
+        if type(cat_id) != int:
+            raise serializers.ValidationError(
+                code=status.HTTP_400_BAD_REQUEST,
+                detail={'error': {'category_id': f'You should give a number!'}}
+            )
+        try:
+            SubCategory.objects.get(id=cat_id)
+        except SubCategory.DoesNotExist:
+            raise serializers.ValidationError(
+                code=status.HTTP_400_BAD_REQUEST,
+                detail={'message': f'Category with id: {cat_id} does not exist!'}
+            )
+        return attrs
 
     def create(self, validated_data):
         product = Product.objects.create(
@@ -19,18 +37,88 @@ class ProductSerializer(ModelSerializer):
             description=validated_data.get('description'),
             category_id=validated_data.get('category_id')
         )
-
+        product = {
+            "id": product.id,
+            "category_id": product.category_id,
+            "name": product.name,
+            "brand": product.brand,
+            "description": product.description,
+            "is_active": product.is_active,
+            "created_at": product.created_at
+        }
         return product
+
+
+class GetProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id', 'category_id', 'name', 'brand',
+            'description', 'is_active', 'created_at'
+        )
+        read_only_fields = fields
+
+
+class UpdateProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'brand', 'description')
+        read_only_fields = ('id', 'created_at')
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.brand = validated_data.get('brand', instance.brand)
         instance.description = validated_data.get('description', instance.description)
-        instance.category_id = validated_data.get('category_id', instance.category_id)
-        instance.is_active = validated_data.get('is_active', instance.is_active)
-        instance.updated_at = timezone.now()
         instance.save()
 
+        product = {
+            "id": instance.id,
+            "category_id": {
+                'id': instance.category_id.id,
+                'name': instance.category_id.name
+            },
+            "name": instance.name,
+            "brand": instance.brand,
+            "description": instance.description,
+            "is_active": instance.is_active,
+            "created_at": instance.created_at
+        }
+        return product
+
+
+class IsActiveStatusProductSerializer(ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'brand', 'description', 'is_active', 'updated_at', 'deleted_at')
+        read_only_fields = ('id', 'name', 'brand', 'description', 'updated_at')
+
+    def validate(self, attrs):
+        is_active = attrs.get('is_active')
+        if type(is_active) != bool:
+            raise serializers.ValidationError(
+                code=status.HTTP_400_BAD_REQUEST,
+                detail={'error': {'category_id': f'You should give a boolean type!'}}
+            )
+        if is_active and self.instance.is_active:
+            raise serializers.ValidationError(
+                detail="Product already activated!"
+            )
+        if not is_active and not self.instance.is_active:
+            raise serializers.ValidationError(
+                detail="Product already deactivated!"
+            )
+        return attrs
+
+    def update(self, instance, validated_data):
+        is_active = validated_data.get('is_active')
+        if is_active:
+            instance.is_active = is_active
+            instance.save()
+        if not is_active:
+            instance.is_active = is_active
+            instance.deleted_at = timezone.now()
+            instance.save()
         return instance
 
 
@@ -90,6 +178,8 @@ class SubCategoryDisableSerializer(ModelSerializer):
 
     def update(self, instance, validated_data):
         instance.is_active = validated_data.get('is_active', instance.is_active)
+        # Task:
+        # It should disable all products depends on this sub category.
         instance.save()
 
         return instance
