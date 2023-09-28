7fbe84331f5837c5b21e6e35964e888a0884bafd
Roman Shyshchenko <shyshchenkoroman@gmail.com>
2023-09-28 11:09:52 +0300

    Edit HasCompleteAddressPermission

--- customer/permissions.py
+++ customer/permissions.py
@@ -13,27 +13,31 @@ class HasCompleteAddressPermission(BasePermission):
 
     def has_permission(self, request, view):
         user = request.user
+        use_new_address = request.data.get("use_new_address")
 
-        if request.data.get("use_new_address") is False:
-            # Checks if the user is using a new address.
-            # If not we will check the existing address
-            if user.is_authenticated:
-                try:
-                    address = user.address  # Get user address by related name
-                except UserAddresses.DoesNotExist:
-                    return False
-
-                if (
-                    address.city
-                    and address.street_address
-                    and address.apartment_address
-                    and address.postal_code
-                ):
-                    return True
-                else:
-                    return False
-
+        if use_new_address is True:
+            address = request.data.get('delivery_address')
+            if (
+                address.get('city')
+                and address.get('street_address')
+                and address.get('apartment_address')
+                and address.get('postal_code')
+            ):  # Checks if address is complete
+                return True
             return False
+
+        if user.is_authenticated:
+            address = user.address  # Get user address by related name
+
+            if (
+                address.city
+                and address.street_address
+                and address.apartment_address
+                and address.postal_code
+            ):
+                return True
+            return False
+        return False
 
 
 class IsEmailVerifiedPermission(BasePermission):
